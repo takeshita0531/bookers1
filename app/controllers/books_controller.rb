30de0b431e1aa3c0187057a761a079e28b7ca195
takeshita0531 <matubara0531@ezweb.ne.jp>
2021-02-15 12:35:54 +0000

    [Finish] 8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,21 +28,25 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book), notice: "Book was successfully update."
+    @books = Book.all
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "Book was successfully update."
+    else
+      render "books/edit"
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path, notice: "Book was successfully destroyde"
+    if book.destroy
+      redirect_to books_path, notice: "Book was successfully destroyde"
+    else
+      redirect_to books_path, notice: "Destroyde error."
+    end
   end
 
   private
-  # def book_params
-  #   params.permit(:title, :body)
-  # end
 
   def book_params
     params.require(:book).permit(:title, :body)
